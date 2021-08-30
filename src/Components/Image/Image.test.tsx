956959feeb9c13fac99c9ffc3e3f0af70957ddc7
Vadim Kasatkin <penguin2804@gmail.com>
2021-08-30 21:49:49 +0400

    Image test

--- src/Components/Image/Image.test.tsx
+++ src/Components/Image/Image.test.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { render, screen } from "@testing-library/react"
-import Image from './Image'
+import { render, screen } from "@testing-library/react";
+import Image from "./Image";
 
-describe('Text component', () => {
-  it('render img', () => {
-    render(<Image src='https://someurl.com/someimg.jpg' alt='simple alt'></Image>)
-    expect((screen.getByAltText('simple alt') as HTMLImageElement).src).toBe('https://someurl.com/someimg.jpg')
-  })
-})
+describe("Text component", () => {
+  it("render img", () => {
+    render(
+      <Image src="https://someurl.com/someimg.jpg" alt="simple alt"></Image>
+    );
+    expect((screen.getByAltText("simple alt") as HTMLImageElement).src).toBe(
+      "https://someurl.com/someimg.jpg"
+    );
+  });
+});
