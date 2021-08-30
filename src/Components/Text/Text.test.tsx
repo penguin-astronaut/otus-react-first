b3eedcb4dc59cb9843eaa81987bc50d5ff08f988
Vadim Kasatkin <penguin2804@gmail.com>
2021-08-30 21:21:32 +0400

    tests for Text component

---
+++ src/Components/Text/Text.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react"
+import Text from "./Text";
+
+describe('Text component', () => {
+  it("simple text", () => {
+    render(<Text>Test Text</Text>);
+    expect(screen.getByText("Test Text")).toBeInTheDocument();
+  });
+  it("cursive text", () => {
+    render(<Text cursive>Test Text</Text>);
+    expect(screen.getByText("Test Text")).toHaveClass('text--cursive');
+  });
+  it("cursive and semi-bold text", () => {
+    render(<Text cursive semiBold>Test Text</Text>);
+    expect(screen.getByText("Test Text")).toHaveClass('text--cursive');
+    expect(screen.getByText("Test Text")).toHaveClass('text--semi-bold');
+  });
+})
--- src/Components/Title/Title.test.tsx
+++ src/Components/Title/Title.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getByText, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Title from "./Title";
 
 describe("Title test", () => {
