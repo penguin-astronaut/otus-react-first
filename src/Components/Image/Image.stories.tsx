e89c7ac9dae420092fb7e56dc293f652fdb18a66
Vadim Kasatkin <penguin2804@gmail.com>
2021-08-31 21:15:31 +0400

    Image Story

---
+++ src/Components/Image/Image.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Meta, Story } from "@storybook/react";
+
+import Image, { IImageProps } from "./Image";
+
+export default {
+  component: Image,
+  title: "Components/Image",
+} as Meta;
+const Template: Story<IImageProps> = (args) => <Image {...args} />;
+
+export const Picture = Template.bind({});
+export const NotFound = Template.bind({});
+
+Picture.args = {
+  src: "https://via.placeholder.com/150",
+  alt: "alt text",
+};
+NotFound.args = {
+  src: "/notfound.png",
+  alt: "alt text using",
+};
