2c45abbbb7df410a3677604041783de9aada839f
Vadim Kasatkin <penguin2804@gmail.com>
2021-09-04 14:16:46 +0400

    Collapse test for click

--- src/Components/CollapseBlock/CollapseBlock.test.tsx
+++ src/Components/CollapseBlock/CollapseBlock.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CollapseBlock from "./CollapseBlock";
 
 describe("CollapseBlock component", () => {
   it("block opened", () => {
     render(<CollapseBlock title="Test title">Inner text</CollapseBlock>);
-    expect(screen.getByText("Inner text")).not.toHaveClass(
+    expect(screen.queryByText("Inner text")).not.toHaveClass(
       "collapse-block__body--collapse"
     );
     expect(
@@ -25,5 +26,17 @@ describe("CollapseBlock component", () => {
     expect(
       screen.getByText("Test title").classList.contains("collapse-block__title")
     ).toBeTruthy();
+  });
+
+  it("change state on header click", () => {
+    render(<CollapseBlock title="Test click title">Inner text</CollapseBlock>);
+    expect(screen.queryByText("Inner text")).not.toHaveClass(
+      "collapse-block__body--collapse"
+    );
+
+    userEvent.click(screen.getByText("Test click title"));
+    expect(screen.queryByText("Inner text")).toHaveClass(
+      "collapse-block__body--collapse"
+    );
   });
 });
